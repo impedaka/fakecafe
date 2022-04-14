dc6d4a12b7b264028aa60f6e608a404df1ea4784
impedaka <qinfengyu123@gmail.com>
2022-04-14 14:43:42 -0400

    a

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import "tailwindcss/tailwind.css";
+import Head from "next/head";
+import { CartProvider } from "@/hooks/use-shopping-cart";
+import { Header, Footer } from "@/components/index";
+import { Toaster } from "react-hot-toast";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CartProvider>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-grow">
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </div>
+      </CartProvider>
+      <Toaster />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    id: "price_1JAE9cEy9j6mAC9ettoD2R0D",
+    name: "Americano",
+    price: 3995,
+    currency: "CAD",
+    image: "/drinks/americano.png",
+    rating: {
+      count: 85,
+      rate: 4.5,
+    },
+  },
+  {
+    id: "price_1JAEABEy9j6mAC9eYTnD6Rv7",
+    name: "Cappuccino",
+    price: 5995,
+    currency: "CAD",
+    image: "/drinks/cappuccino.png",
+    rating: {
+      count: 137,
+      rate: 4.5,
+    },
+  },
+];
+
+export default products;
