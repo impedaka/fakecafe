55267bf283508fe5c3de88d8047ae8a352fc20d9
Alice Yu <75595719+impedaka@users.noreply.github.com>
2022-05-30 16:24:23 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,56 @@
-import NextLink from "next/link";
-
 import {
-  Container,
   Stack,
   Flex,
-  Box,
-  Heading,
-  Text,
   Button,
-  Image,
-  Icon,
-  IconButton,
-  createIcon,
-  IconProps,
-  useColorModeValue,
+  Text,
+  VStack,
+  useBreakpointValue,
 } from "@chakra-ui/react";
-import Section from "@/components/Section";
-
-export default function Homepage() {
+import Link from "next/link";
+import Navbar from "../Components/Navbar";
+import Cart from "./cart";
+import { useState, useContext } from "react";
+export default function WithBackgroundImage() {
   return (
-    <Container maxW={"7xl"}>
-      <Stack
-        align={"center"}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}
-        direction={{ base: "column", md: "row" }}
+    <Flex
+      w={"full"}
+      h={"100vh"}
+      backgroundImage={
+        "url(https://static.dezeen.com/uploads/2018/06/hono-izakaya-charlene-bourgeois-interiors-restaurant-quebec-city-canada_dezeen_2364_col_0.jpg)"
+      }
+      backgroundSize={"cover"}
+      backgroundPosition={"center center"}
+    >
+      <Navbar ree="white" />
+      <VStack
+        w={"full"}
+        justify={"center"}
+        align={"flex-start"}
+        px={useBreakpointValue({ base: 10, md: 20 })}
+        bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
       >
-        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-          <Heading
-            lineHeight={1.1}
-            fontWeight={600}
-            fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+        <Stack maxW={"3xl"} align={"flex-start"} spacing={6}>
+          <Text
+            color={"white"}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: "5xl", md: "6xl" })}
           >
-            <Text as={"span"} position={"relative"}>
-              FakeCafe
-            </Text>
-            <br />
-            <Text as={"span"} color={"black"}>
-              100% Not Real!
-            </Text>
-          </Heading>
-          <Text color={"gray.800"}>
-            A café is a type of restaurant which typically serves coffee and
-            tea, in addition to light refreshments such as baked goods or
-            snacks. However, fakecafe is a café that provides none of that.
+            Starbucks Coffee Company
           </Text>
-          <Stack
-            spacing={{ base: 4, sm: 6 }}
-            direction={{ base: "column", sm: "row" }}
+          <Text color="white" fontSize={"2xl"}>
+            Hot Coffees - Cold Drinks - Hot Drinks - Bakery - Hot Breakfast
+          </Text>
+          <Button
+            size="lg"
+            bg={"white"}
+            color={"black"}
+            _hover={{ bg: "grey.200" }}
           >
-            <NextLink href="/menu">
-              <Button
-                size={"lg"}
-                fontWeight={"normal"}
-                px={6}
-                bg="black"
-                color="white"
-                _hover={{ bg: "blackAlpha.700" }}
-              >
-                Order Now!
-              </Button>
-            </NextLink>
-          </Stack>
+            <Link href="/menu">Order Now</Link>
+          </Button>
         </Stack>
-        <Flex
-          flex={1}
-          justify={"center"}
-          align={"center"}
-          position={"relative"}
-          w={"full"}
-        >
-          <Box
-            position={"relative"}
-            height={"300px"}
-            rounded={"2xl"}
-            boxShadow={"2xl"}
-            width={"full"}
-            overflow={"hidden"}
-          >
-            <Image
-              alt={"Hero Image"}
-              fit={"cover"}
-              align={"center"}
-              w={"100%"}
-              h={"100%"}
-              src={
-                "https://static.dezeen.com/uploads/2018/06/hono-izakaya-charlene-bourgeois-interiors-restaurant-quebec-city-canada_dezeen_2364_col_0.jpg"
-              }
-            />
-          </Box>
-        </Flex>
-      </Stack>
-    </Container>
+      </VStack>
+    </Flex>
   );
 }
